5e7fbb455421307cce07ea7c215db5fd38a62402
gitking-han <hanzalarehman1000@gmail.com>
2025-04-19 19:26:11 +0500

    del text-Utils folder with new content

--- text-Utils/module1.mjs
+++
@@ -1,6 +0,0 @@
-const a = "harry";
-const b = "huzaifa";
-const c = "hanzala";
-const d = "rohan";
-export default c;
-export { a, b, d };
--- text-Utils/module2.mjs
+++
@@ -1,5 +0,0 @@
-import ui, {a, b, d} from './module1.mjs';
-console.log(ui);
-console.log(a);
-console.log(b);
-console.log(d);
